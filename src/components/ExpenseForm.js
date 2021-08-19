a9b0d1cb3c7297db767e9e71c8c28113529fbbc6
TatyCoder <tmilanes@gmail.com>
2021-08-19 15:50:08 -0400

    Handling form submission

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,21 +1,22 @@
 import './ExpenseForm.css';
 import { useState } from 'react';
 
+// Switching back to the multiple states approach:
 const ExpenseForm = () => {
     // I can have multiple states inside of the same component, managing and updating them separately:
-    // const [enteredTitle, setEnteredTitle] = useState('');  
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('');
+    const [enteredTitle, setEnteredTitle] = useState('');  
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
     // Using one state instead, passing in an object as a value, grouping the 3 states and updating all 3 properties:
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
-    });
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // });
 
     const titleChangeHandler = (event) => {
-        // setEnteredTitle(event.target.value);  
+        setEnteredTitle(event.target.value);  
 
         // Now calling the setUserInput passing in a new object.
         // I need to use the spread operator to copy the other two key value pairs which I'm not updating here:
@@ -25,28 +26,44 @@ const ExpenseForm = () => {
         // });
 
         // Passing a function to setUserInput*:
-        setUserInput((prevState) => {
-            return { ...prevState, enteredTitle: event.target.value }
-        });
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value }
+        // });
     };
 
     const amountChangeHandler = (event) => {
-        // setEnteredAmount(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value,
-        });
+        setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // });
     };
 
     const dateChangeHandler = (event) => {
-        // setEnteredDate(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value,
-        });
+        setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // });
     };
 
-    return <form>
+    // A function whenever the form is being submitted:
+    const submitHandler = (event) => {
+        event.preventDefault(); // To prevent the default behavior*
+
+        // To create an object that combines all the entered data:
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate) // Dates are tricky**
+        };
+
+        console.log(expenseData);
+    };
+
+    // Now I wanna make sure that this form can be submitted when the button is pressed:
+    return (
+        <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
@@ -65,13 +82,13 @@ const ExpenseForm = () => {
             <button type="submit">Add Expense</button>
         </div>
     </form>
+    );
 };
 
 export default ExpenseForm;
 
-/* Notes: *whenever I update my state and it depends on the previous state, I 
-should call the setUserInput function and pass a function to it. This function 
-will automatically be executed by React and it will receive the previous state 
-snapshot for that state for which I'm calling the updating function. With this  
-approach, React will guarantee that the state snapshot it gives me in this inner 
-function, will always be the latest state snapshot. */
+/* Notes: *the default browser behavior is that if I click the button, 
+the page reloads because the browser automatically sends a request. 
+**constructing a new date with the built in date constructor to 
+which then I passed the enteredDate, which will parse that date 
+string and converted into a date object. */
