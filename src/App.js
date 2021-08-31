529268c7e4ca7d76a798ef51a268c3d0b82a08b5
TatyCoder <tmilanes@gmail.com>
2021-08-31 14:36:53 -0400

    Rendering lists of data with map and useState

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
-// import ExpenseItem from './components/ExpenseItem';  // Not using this anymore.
+// Now importing useState:
+import { useState } from 'react';
 import Expenses from './components/Expenses';
 import NewExpense from './components/NewExpense';
 
 // The goal will be to add the new expense which the user entered to this list of existing expenses.
-// So, I need to pass the data which I'm collecting and generating in ExpenseForm to the app component:
-const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { 
-      id: 'e2', 
-      title: 'New TV', 
-      amount: 799.49, 
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450.99,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+// Extracting this constant out of the App component:
+const expensesList = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { 
+    id: 'e2', 
+    title: 'New TV', 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450.99,
+    date: new Date(2021, 5, 12),
+  },
+];
 
-  // Adding a new function:
+// Now inside of the App component function I call useState:
+const App = () => {
+  const [expenses, setExpenses] = useState(expensesList);
+
   const addExpenseHandler = expense => {
-    console.log('In App.js');  // To confirm that the data arrived, I will not update the expenses array yet.
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    });
     console.log(expense);
   };
 
-  // Adding a new prop (name of my choice, starting with on...) to NewExpense:
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -13,11 +13,23 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     };
 
+    // To render the list dynamically using map() and getting rid of the hard coded ExpenseItems here:
     return (
         <div>
             <Card className="expenses">
-                <ExpensesFilter onSelected={filteredYear} onYearFilter={selectYearHandler}/>
-                <ExpenseItem
+                <ExpensesFilter 
+                    onSelected={filteredYear} 
+                    onYearFilter={selectYearHandler}
+                />
+                {props.items.map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title} 
+                        amount={expense.amount} 
+                        date={expense.date}
+                    />
+                ))}
+                {/* <ExpenseItem
                     title={props.items[0].title}
                     amount={props.items[0].amount}
                     date={props.items[0].date}
@@ -36,10 +48,17 @@ const Expenses = (props) => {
                     title={props.items[3].title}
                     amount={props.items[3].amount}
                     date={props.items[3].date}
-                ></ExpenseItem>
+                ></ExpenseItem> */}
             </Card>
         </div>
     );
 }
 
 export default Expenses;
+
+/* Notes: map() creates a new array based on another array, so it basically 
+transforms every element in that original array. So map() takes a function 
+as a argument, and that function executes for every element in the array, 
+and the result of this function is the element which will be added to the 
+newly created array. With this map expression here it transforms the array  
+to an array full of JSX items. Always add key when mapping out lists of items.*/
